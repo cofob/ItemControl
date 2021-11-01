7fd987b0a552c066622f45c1d909d9f63cc41a54
cofob <cofob@riseup.net>
2021-11-01 12:32:18 +0700

    Release v0.0.1

---
+++ src/main/java/ru/cofob/ItemControl/ItemControl.java
@@ -0,0 +1,34 @@
+package ru.cofob.ItemControl;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+import ru.cofob.ItemControl.listener.InventoryOpen;
+
+import java.util.logging.Logger;
+
+
+public class ItemControl extends JavaPlugin {
+    public String version;
+    private final Logger log;
+
+    public ItemControl() {
+        this.log = this.getLogger();
+    }
+
+    @Override
+    public void onEnable() {
+//        defining variables
+        version = "v0.0.1";
+        log.info("§a[ItemControl] §rLoaded §6"+version+"§r!");
+        new InventoryOpen(this).register();
+    }
+
+    public boolean onCommand(CommandSender s, Command cmd, String label, String[] args) {
+        if(cmd.getName().equalsIgnoreCase("itemcontrol")) {
+            s.sendMessage("§a[ItemControl] §rversion: §6"+version+"§r!");
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/main/java/ru/cofob/ItemControl/listener/InventoryOpen.java
@@ -0,0 +1,62 @@
+package ru.cofob.ItemControl.listener;
+
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.inventory.ItemStack;
+import ru.cofob.ItemControl.ItemControl;
+
+import java.util.logging.Logger;
+
+import static org.bukkit.Bukkit.getServer;
+
+public class InventoryOpen {
+    private final ItemControl plugin;
+
+    public InventoryOpen(ItemControl plugin) {
+        this.plugin = plugin;
+    }
+
+    public void register() {
+        getServer().getPluginManager().registerEvents(new EventsListener(this.plugin), this.plugin);
+    }
+
+    private static class EventsListener implements Listener {
+        private final Logger log;
+
+        public EventsListener(ItemControl plugin) {
+            this.log = plugin.getLogger();
+            log.info("§a[ItemControl] §6debug§r: registered InventoryOpenEvent.");
+        }
+
+        @EventHandler
+        public void onInventoryOpenEvent(InventoryOpenEvent  e) {
+            HumanEntity player = e.getPlayer();
+            for(ItemStack item : player.getInventory().getContents())
+            {
+                if(item != null)
+                {
+                    for(Enchantment enchantment : item.getEnchantments().keySet()) {
+                        log.info(item.toString());
+                        int level = item.getEnchantmentLevel(enchantment);
+                        int max_level = enchantment.getMaxLevel();
+                        boolean is_converted = false;
+                        boolean breaking = false;
+                        if(level > max_level) {
+                            item.removeEnchantment(enchantment);
+                            if(enchantment.canEnchantItem(item)){
+                                item.addEnchantment(enchantment, max_level);
+                                breaking = true;
+                            }
+                            is_converted = true;
+                        }
+                        log.info("§a[ItemControl] §6debug§r: name: "+ enchantment.getKey() + ", current level: " + level + ", max level: " + max_level + ", is converted? " + is_converted + ", breaking? " + breaking);
+                    }
+                }
+            }
+        }
+    }
+}
